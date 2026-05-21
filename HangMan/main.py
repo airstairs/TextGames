96747bc94ef8c058f8e44e25fd4941c68267c250
xthan <hackattendant@yahoo.com>
2026-05-20 20:36:05 -0400

    hangman dead tracker add non repeat guesses and head torso larm rarm lleg rlet ascii animation print outs static

--- HangMan/main.py
+++ HangMan/main.py
@@ -1,1 +1,28 @@
+import os
+
 print("welcome to hangman")
+phrase = input("please enter your phrase without punctuation \nphrase: ")
+dead ="_______"
+place=""
+go = True
+while go:
+   os.system("clear")
+   print(dead)
+   print(place)
+   for char in phrase:
+       if char != " ":
+           print("-", end="")
+
+       else:
+           print(" ",end="")
+
+   print("\nEnter guess: ")
+   guess=input()
+   if guess not in phrase:
+       place += "_"
+   if place == dead:
+       go=False
+       os.system("clear")
+       print(dead)
+       print(place)
+       print("You died!")
