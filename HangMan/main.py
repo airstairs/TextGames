923e079474881c62aa7d0bb8a5d9c584dc430e03
xthan <hackattendant@yahoo.com>
2026-05-20 21:01:31 -0400

    only lowercase hangman nobody animstion just orogress bar

--- HangMan/main.py
+++ HangMan/main.py
@@ -4,25 +4,38 @@ print("welcome to hangman")
 phrase = input("please enter your phrase without punctuation \nphrase: ")
 dead ="_______"
 place=""
+cgs=""
 go = True
 while go:
    os.system("clear")
+   solved = ""
    print(dead)
    print(place)
    for char in phrase:
        if char != " ":
-           print("-", end="")
-
+           if char in cgs:
+               solved += char
+           else:
+               solved += "-" 
        else:
-           print(" ",end="")
+           solved += " "
 
+   print(solved)
    print("\nEnter guess: ")
    guess=input()
    if guess not in phrase:
        place += "_"
+   if guess in phrase:
+       cgs += guess
    if place == dead:
        go=False
        os.system("clear")
        print(dead)
        print(place)
        print("You died!")
+   if solved == phrase:
+       go = False
+       os.system("clear")
+       os.system(f"figlet you win! | lolcat")
+       print(phrase)
+
