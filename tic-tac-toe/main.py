7c5975ffa9d9b2e50db0cfe88ed79581f39f5ad0
xthan <hackattendant@yahoo.com>
2026-06-18 22:01:55 -0400

    print board

---
+++ tic-tac-toe/main.py
@@ -0,0 +1,32 @@
+import numpy as np
+import os
+import time
+
+x1 = "   #"
+x2 = "   #"
+x3 = "    "
+
+sep= "############"
+
+x4 = "   #"
+x5 = "   #"
+x6 = "    "
+
+sep= "############"
+
+x7 = "   #"
+x8 = "   #"
+x9 = "    "
+
+
+def print_board():
+    print(x1+x2+x3)
+    print(sep)
+    print(x4+x5+x6)
+    print(sep)
+    print(x7+x8+x9)
+
+
+
+if __name__ == "__main__":
+    print_board()
